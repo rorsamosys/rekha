d9c50d5b7be597612249e81067f0da036ea93009
Vaibhav Joshi <vaibhavjoshi@cdnsol.com>
2014-09-26 11:54:41 +0530

    first commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CdnShop::Application.config.session_store :cookie_store, key: '_cdn_shop_session'
---
+++ config/initializers/spree.rb
@@ -0,0 +1,17 @@
+# Configure Spree Preferences
+#
+# Note: Initializing preferences available within the Admin will overwrite any changes that were made through the user interface when you restart.
+#       If you would like users to be able to update a setting with the Admin it should NOT be set here.
+#
+# In order to initialize a setting do:
+# config.setting_name = 'new value' 
+Spree.config do |config|
+  # Example:
+  # Uncomment to override the default site name.
+  # config.site_name = "Spree Demo Site"
+  config.site_name = 'Cdn Online Shop'
+  config.admin_interface_logo = "/assets/cdn-logo.jpg" 
+  config.logo = "/assets/cdn-logo.jpg" 
+end
+
+Spree.user_class = "Spree::User"
